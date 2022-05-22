7abe54481fe53dbb618c54cbcdeb3d45fceedb54
thisisabhinay <a8h1nay@gmail.com>
2022-05-23 00:10:39 +0530

    Establishes base blog

--- pages/posts/first.tsx
+++ pages/posts/first.tsx
@@ -1,16 +1,21 @@
 import type { NextPage } from 'next'
+import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../../components/layout';
 
 const First: NextPage = () => {
     return (
-    <>
-        <h1>First Post</h1>
-        <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-    </>
+        <Layout>
+            <Head>
+                <title>First Page XO</title>
+            </Head>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">
+                    <a>Back to home</a>
+                </Link>
+            </h2>
+        </Layout>
     );
 }
 
